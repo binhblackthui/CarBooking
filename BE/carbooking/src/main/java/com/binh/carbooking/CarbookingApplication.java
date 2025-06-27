3e8e8580a61e2bfd9ce932defc22f4984ebf2260
binhblackthui <binhblacktui@gmail.com>
2025-06-27 21:51:15 +0700

    Add Spring Boot backend

---
+++ BE/carbooking/src/main/java/com/binh/carbooking/CarbookingApplication.java
@@ -0,0 +1,13 @@
+package com.binh.carbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbookingApplication.class, args);
+	}
+
+}
---
+++ BE/carbooking/src/test/java/com/binh/carbooking/CarbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binh.carbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
